90ab437996baf735328f00ccc83ce6e5612616ae
eduardorascon <eduardo.rascon@gmail.com>
2017-03-04 14:54:04 -0600

    Fields name and description added to the database migration.

--- database/migrations/2017_03_04_204218_create_roles_table.php
+++ database/migrations/2017_03_04_204218_create_roles_table.php
@@ -16,6 +16,8 @@ class CreateRolesTable extends Migration
         Schema::create('roles', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
+            $table->string('name');
+            $table->string('description');
         });
     }
 
