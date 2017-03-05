a0feb7f49c881e196eb3457e0603da38266d3893
eduardorascon <eduardo.rascon@gmail.com>
2017-03-04 18:33:36 -0600

    Roles added to RoleTableSeeder.

--- database/seeds/RoleTableSeeder.php
+++ database/seeds/RoleTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Role;
 
 class RoleTableSeeder extends Seeder
 {
@@ -11,6 +12,19 @@ class RoleTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $role_user = new Role();
+        $role_user->name = 'User';
+        $role_user->description = 'A regular user';
+        $role_user->save();
+
+        $role_admin = new Role();
+        $role_admin->name = 'Admin';
+        $role_admin->description = 'An administration user';
+        $role_admin->save();
+
+        $role_salesman = new Role();
+        $role_salesman->name = 'Salesman';
+        $role_salesman->description = 'A salesman';
+        $role_salesman->save();
     }
 }
