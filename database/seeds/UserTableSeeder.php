b691fd4c682cb9b427c4f17756ce78510ffaf788
eduardorascon <eduardo.rascon@gmail.com>
2017-03-04 18:42:18 -0600

    Users added to UserTableSeeder.

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
 
 class UserTableSeeder extends Seeder
 {
@@ -11,6 +12,25 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $user = new User();
+        $user->first_name = 'Luis';
+        $user->last_name = 'Rascon';
+        $user->email = 'luis@ejemplo.com';
+        $user->password = bcrypt('user');
+        $user->save();
+
+        $admin = new User();
+        $admin->first_name = 'Marco';
+        $admin->last_name = 'Quezada';
+        $admin->email = 'marco@ejemplo.com';
+        $admin->password = bcrypt('admin');
+        $admin->save();
+
+        $salesman = new User();
+        $salesman->first_name = 'Jorge';
+        $salesman->last_name = 'Mendoza';
+        $salesman->email = 'jorge@ejemplo.com';
+        $salesman->password = bcrypt('salesman');
+        $salesman->save();
     }
 }
