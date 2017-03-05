a818f79aeb2970005129fb576519c24606329355
JorgeMendozaG <jorge.mendoza.guillen@gmail.com>
2017-03-05 10:52:11 -0700

    Migrations and seeders for clients and breeds (catalogs) added

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        //USED
+        //$this->call(BreedsTableSeeder::class);
+        //$this->call(ClientsTableSeeder::class);
+
     }
 }
