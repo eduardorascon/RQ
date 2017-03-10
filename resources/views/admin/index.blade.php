f2611c40f6d6b15dca5930e5d0b121d5aab42a75
eduardorascon <eduardo.rascon@gmail.com>
2017-03-09 20:53:45 -0600

    Form added for creating new users.

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -12,6 +12,46 @@
                 </div>
             </div>
 
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    Agregar un nuevo usuario
+                </div>
+
+                <div class="panel-body">
+                    <!-- New User Form -->
+                    <form action="{{ url('admin.create_new_user')}}" method="POST" class="form-horizontal">
+                        {{ csrf_field() }}
+
+                        <!-- User Name -->
+                        <div class="form-group">
+                            <label for="user-name" class="col-sm-3 control-label">Nombre</label>
+
+                            <div class="col-sm-6">
+                                <input type="text" name="name" id="user-name" class="form-control" value="{{ old('name') }}">
+                            </div>
+                        </div>
+
+                        <!-- User Name -->
+                        <div class="form-group">
+                            <label for="user-email" class="col-sm-3 control-label">Correo electrónico</label>
+
+                            <div class="col-sm-6">
+                                <input type="text" name="email" id="user-email" class="form-control" value="{{ old('email') }}">
+                            </div>
+                        </div>
+
+                        <!-- Add User Button -->
+                        <div class="form-group">
+                            <div class="col-sm-offset-3 col-sm-6">
+                                <button type="submit" class="btn btn-default">
+                                    <i class="fa fa-btn fa-plus"></i>Agregar usuario
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
             @if (count($users) > 0)
             <div class="panel panel-default">
                 <div class="panel-heading">
