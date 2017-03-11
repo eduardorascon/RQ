27b68e09905369fd15b8d610e4888fb156e529e4
Jorge Mendoza <jorge.mendoza.guillen@gmail.com>
2017-03-11 13:59:16 -0700

    More style

--- resources/views/clients/edit.blade.php
+++ resources/views/clients/edit.blade.php
@@ -2,47 +2,63 @@
 
 @section('content')
 <div class="container">
+  <div class="row">
+    <div class="col-md-8 col-md-offset-2">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          Editar
+        </div>        
+        <div class="panel-body">
+          <form class="form-horizontal" action="{{route('clients.update',$client->id)}}" method="post">
+            <input name="_method" type="hidden" value="PATCH">
+            {{csrf_field()}}      
 
-   <div class="row">
-      <div class="col-md-12">
-        <h1>Editar</h1>
+            <div class="form-group">
+              <label class="col-sm-2 control-label" for="first_name">Nombre:</label>
+              <div class="col-sm-10">
+                <input type="text" name="first_name" class="form-control" placeholder="Nombre" value="{{$client->first_name}}">
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label class="col-sm-2 control-label" for="last_name">Apellidos:</label>
+              <div class="col-sm-10">
+                <input type="text" name="last_name" class="form-control" placeholder="Apellidos" value="{{$client->last_name}}">        
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label class="col-sm-2 control-label" for="address">Dirección:</label>
+              <div class="col-sm-10">
+                <input type="text" name="address" class="form-control" placeholder="Dirección" value="{{$client->address}}">       
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label class="col-sm-2 control-label" for="company">Empresa: </label>
+              <div class="col-sm-10"> 
+                <input type="text" name="company" class="form-control" placeholder="Empresa" value="{{$client->company}}">
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label class="col-sm-2 control-label" for="phone">Teléfono</label>
+              <div class="col-sm-10">
+                <input type="text" name="phone" class="form-control" placeholder="Teléfono" value="{{$client->phone}}">
+              </div>
+            </div>
+
+            <div class="form-group">
+              <div class="col-sm-offset-2 col-sm-10">
+                <input type="submit" class="btn btn-primary" value="Guardar">
+              </div>
+            </div>
+
+          </form>
+
+        </div>
       </div>
     </div>
-    <div class="row">
-      <form class="" action="{{route('clients.update',$client->id)}}" method="post">
-        <input name="_method" type="hidden" value="PATCH">
-        {{csrf_field()}}      
-        
-        <div>
-           <label for="first_name">Nombre:</label>
-           <input type="text" name="first_name" class="form-control" placeholder="Nombre" value="{{$client->first_name}}">        
-         </div>
-   
-         <div>
-         <label for="last_name">Apellidos:</label>
-           <input type="text" name="last_name" class="form-control" placeholder="Apellidos" value="{{$client->last_name}}">        
-         </div>
-         
-         <div>
-           <label for="address">Dirección:</label>
-           <input type="text" name="address" class="form-control" placeholder="Dirección" value="{{$client->address}}">       
-         </div>
-   
-         <div>
-           <label for="company">Empresa: </label>
-           <input type="text" name="company" class="form-control" placeholder="Empresa" value="{{$client->company}}">
-         </div>
-   
-         <div>
-           <label for="phone">Teléfono</label>
-           <input type="text" name="phone" class="form-control" placeholder="Teléfono" value="{{$client->phone}}">
-        </div>
-
-        <div>
-          <input type="submit" class="btn btn-primary" value="Guardar">
-        </div>
-
-      </form>
-    </div>
+  </div>
 </div>
 @endsection
