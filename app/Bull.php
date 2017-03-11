7b5bea101da6476793ae6cdd02e2d4bf2497d0a8
eduardorascon <eduardo.rascon@gmail.com>
2017-03-11 16:07:00 -0600

    Models bull, cow and calf now extend the Cattle model.

--- app/Bull.php
+++ app/Bull.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Bull extends Model
+class Bull extends Cattle
 {
-    //
+	//
 }
--- app/Calf.php
+++ app/Calf.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Calf extends Model
+class Calf extends Cattle
 {
     //
 }
