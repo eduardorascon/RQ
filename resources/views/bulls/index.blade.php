d7743e0d0cc97af0ae24756a451269e530fa915f
eduardorascon <eduardo.rascon@gmail.com>
2017-03-12 17:28:10 -0600

    Index view now displays data from the database.

--- resources/views/bulls/index.blade.php
+++ resources/views/bulls/index.blade.php
@@ -10,6 +10,30 @@
 					<a href="{{ route('bulls.create') }}">Agregar nuevo toro</a>
 				</div>
 				<div class="panel-body">
+					<div class="table-responsive">
+						<table class="table table-striped">
+						<thead>
+							<tr>
+								<th>Etiqueta</th>
+								<th>Fecha de compra</th>
+								<th>Fecha de nacimiento</th>
+								<th>Raza</th>
+								<th></th>
+							</tr>
+						</thead>
+						<tbody>
+						@foreach($bulls as $bull)
+							<tr>
+								<td>{{ $bull->cattle->tag }}</td>
+								<td>{{ $bull->cattle->purchase_date }}</td>
+								<td>{{ $bull->cattle->birth }}</td>
+								<td>{{ $bull->cattle->breed->name }}</td>
+								<td></td>
+							</tr>
+						@endforeach
+						</tbody>
+						</table>
+					</div>
 				</div>
 			</div>
 		</div>
