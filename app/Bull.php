8096f70d33695b8b13a3685b0a8cb6db82fdd0c2
eduardorascon <eduardo.rascon@gmail.com>
2017-03-12 16:33:52 -0600

    Bull model extends Model instead of the Cattle model.

--- app/Bull.php
+++ app/Bull.php
@@ -4,7 +4,10 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Bull extends Cattle
+class Bull extends Model
 {
-	//
+	public function cattle()
+	{
+		return $this->hasOne('App\Cattle');
+	}
 }
