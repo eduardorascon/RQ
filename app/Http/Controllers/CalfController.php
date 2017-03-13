c63205ead88fdb4bd6f6072cb64f66fae972264c
eduardorascon <eduardo.rascon@gmail.com>
2017-03-12 18:10:44 -0600

    Missing imports added to the Controllers.

--- app/Http/Controllers/CalfController.php
+++ app/Http/Controllers/CalfController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Calf;
+use App\Cattle;
+use App\Breed;
 
 class CalfController extends Controller
 {
--- app/Http/Controllers/CowController.php
+++ app/Http/Controllers/CowController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Cow;
+use App\Cattle;
+use App\Breed;
 
 class CowController extends Controller
 {
