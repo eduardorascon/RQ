d7fafd4210f844341b37c705781333ae8ee5cd70
eduardorascon <eduardo.rascon@gmail.com>
2017-03-16 22:24:50 -0600

    Views modified to increase usable space.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-            </div>
+        <div class="col-md-offset-1 col-md-10">
         </div>
     </div>
 </div>
