cd09d0b1a325f55924583d6b31d9839c2ab70c24
eduardorascon <eduardo.rascon@gmail.com>
2017-03-22 20:23:41 -0600

    Rectal palpation form added.

--- resources/views/cows/show.blade.php
+++ resources/views/cows/show.blade.php
@@ -192,6 +192,36 @@
 				@endif
 			</div>
 
+			<div class="panel panel-default">
+				<div class="panel-heading">Registro de palpaciones</div>
+				<div class="panel-body">
+					<form class="form-inline" action="" method="post">
+						{{ csrf_field() }}
+						<div class="row">
+							<div class="col-sm-10">
+								<div class="form-group col-sm-5">
+								<label class="col-sm-2" for="months">Meses</label>
+								<input type="number" step="any" class="form-control col-sm-offset-2 col-sm-2" name="months" id="months" placeholder="Meses">
+								</div>
+
+								<div class="form-group col-sm-5">
+								<label class="col-sm-2" for="date">Fecha</label>
+								<input type="date" class="form-control col-sm-offset-2 col-sm-2" name="date" id="date" placeholder="Fecha">
+								</div>
+
+								<div class="form-group col-sm-5">
+								<label class="col-sm-2" for="comment">Comentario</label>
+								<input type="text" class="form-control col-sm-offset-2 col-sm-2" name="comment" id="comment" placeholder="Comentario">
+								</div>
+							</div>
+							<div class="col-sm-2">
+								<button type="submit" class="btn btn-default">Guardar</button>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+
 		</div>
 	</div>
 </div>
