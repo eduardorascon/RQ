38d4f39e17a354790688999b59a0a96ba1be9449
eduardorascon <eduardo.rascon@gmail.com>
2017-03-26 09:36:25 -0600

    Sample gallery created.

--- resources/views/bulls/show.blade.php
+++ resources/views/bulls/show.blade.php
@@ -55,6 +55,21 @@
 						</div>
 					</form>
 				</div>
+				<div class="panel-body">
+			        <div class="row">
+			            <a href="https://unsplash.it/1200/768.jpg?image=251" data-toggle="lightbox" data-gallery="example-gallery" class="col-sm-2">
+			                <img src="https://unsplash.it/600.jpg?image=251" class="img-responsive">
+			            </a>
+			            <a href="https://unsplash.it/1200/768.jpg?image=252" data-toggle="lightbox" data-gallery="example-gallery" class="col-sm-2">
+			                <img src="https://unsplash.it/600.jpg?image=252" class="img-responsive">
+			            </a>
+			            <a href="https://unsplash.it/1200/768.jpg?image=253" data-toggle="lightbox" data-gallery="example-gallery" class="col-sm-2">
+			                <img src="https://unsplash.it/600.jpg?image=253" class="img-responsive">
+			            </a>
+			        </div>
+
+
+				</div>
 			</div>
 
 			<div class="panel panel-default">
