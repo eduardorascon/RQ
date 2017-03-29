3e81af1fd64f9278e9bae436f990796e1a4dd2e2
eduardorascon <eduardo.rascon@gmail.com>
2017-03-28 22:35:18 -0600

    Picture placeholders deleted.

--- resources/views/bulls/show.blade.php
+++ resources/views/bulls/show.blade.php
@@ -68,17 +68,6 @@
 			            @endforeach
 					</div>
 					@endif
-			        <div class="row">
-			            <a href="https://unsplash.it/1200/768.jpg?image=251" data-toggle="lightbox" data-gallery="example-gallery" class="col-sm-2">
-			                <img src="https://unsplash.it/600.jpg?image=251" class="img-responsive">
-			            </a>
-			            <a href="https://unsplash.it/1200/768.jpg?image=252" data-toggle="lightbox" data-gallery="example-gallery" class="col-sm-2">
-			                <img src="https://unsplash.it/600.jpg?image=252" class="img-responsive">
-			            </a>
-			            <a href="https://unsplash.it/1200/768.jpg?image=253" data-toggle="lightbox" data-gallery="example-gallery" class="col-sm-2">
-			                <img src="https://unsplash.it/600.jpg?image=253" class="img-responsive">
-			            </a>
-			        </div>
 				</div>
 			</div>
 
