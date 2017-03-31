e1bb2cf645cc99d7660a8ad0a5de7989188457e0
eduardorascon <eduardo.rascon@gmail.com>
2017-03-30 23:15:06 -0600

    Semi functional search available.

--- app/Http/Controllers/CalfSaleController.php
+++ app/Http/Controllers/CalfSaleController.php
@@ -3,11 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Calf;
 
 class CalfSaleController extends Controller
 {
     public function index()
     {
-    	return vieW('calves_sales.index');
+    	$calves = Calf::all();
+
+		$is_search = isset($_GET['s']);
+    	if($is_search)
+    	{
+    		$calves = Calf::whereHas('cattle', function ($q) {
+    			$q->where('tag', 'LIKE', '%' . $_GET['s'] . '%');
+    		})->get();
+    	}
+
+    	return view('calves_sales.index', ['calves' => $calves]);
     }
 }
--- resources/views/calves_sales/index.blade.php
+++ resources/views/calves_sales/index.blade.php
@@ -6,6 +6,27 @@
 		<div class="col-md-offset-1 col-md-10">
 			<div class="panel panel-default">
 				<div class="panel-heading">Ventas</div>
+				<div class="panel-body">
+
+					<div class="row">
+						<div class="col-lg-6">
+							<div class="input-group">
+								<input type="text" class="form-control" placeholder="Buscar...">
+								<span class="input-group-btn">
+									<button class="btn btn-default btn-info" type="button">Buscar</button>
+								</span>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+
+			<div class="panel panel-default">
+				<div class="panel-heading">Becerros ({{ count($calves) }})</div>
+				<div class="panel-body">
+					
+				</div>
 			</div>
 		</div>
 	</div>
