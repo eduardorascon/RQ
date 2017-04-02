c87c4c6aeab1faba356b732e94d01b8ad26b434c
eduardorascon <eduardo.rascon@gmail.com>
2017-04-01 21:13:48 -0600

    View calves_sales/show.blade.php created.

--- app/CalfSale.php
+++ app/CalfSale.php
@@ -8,4 +8,10 @@ class CalfSale extends Model
 {
     protected $table = 'calves_sales';
     public $timestamps = false;
+
+    public function getSaleDateAttribute($value)
+    {
+        setLocale(LC_TIME, 'es_MX.UTF-8', 'Spanish_Spain.1252');
+        return Carbon::parse($value)->formatLocalized('%d/%B/%Y');
+    }
 }
