49955e1a8286664b0bfb1440958bf30a4c189903
eduardorascon <eduardo.rascon@gmail.com>
2017-04-01 19:32:29 -0600

    create.blade modified.

--- resources/views/calves_sales/create.blade.php
+++ resources/views/calves_sales/create.blade.php
@@ -1,4 +1,48 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container">
+	<div class="row">
+		<div class="col-md-offset-1 col-md-10">
+			<div class="panel panel-default">
+				<div class="panel-heading">Registro de venta</div>
+				<div class="panel-body">
+				<form class="form-horizontal" action="" method="post">
+					{{ csrf_field() }}
+
+					<div class="form-group">
+					<label class="col-sm-2 control-label" for="sale_date">Fecha</label>
+					<div class="col-sm-10">
+						<input type="date" name="sale_date" class="form-control" placeholder="Fecha...">
+					</div>
+					</div>
+
+					<div class="form-group">
+					<label class="col-sm-2 control-label" for="sale_weight">Peso</label>
+					<div class="col-sm-10">
+						<input type="number" step="any" name="sale_weight" class="form-control" placeholder="Peso...">
+					</div>
+					</div>
+
+					<div class="form-group">
+					<label class="col-sm-2 control-label" for="sale_price_per_kilo">Precio por kilo</label>
+					<div class="col-sm-10">
+						<div class="input-group">
+						<div class="input-group-addon">$</div>
+						<input type="number" step="any" name="sale_price_per_kilo" class="form-control" placeholder="Precio por kilo...">
+						</div>
+					</div>
+					</div>
+
+					<div class="form-group">
+					<div class="col-sm-offset-2 col-sm-10">
+						<input type="submit" class="btn btn-primary" value="Guardar">
+					</div>
+					</div>
+				</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 @endsection
