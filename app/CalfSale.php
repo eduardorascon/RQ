449149c7bb2e5180b1b79d2781d735d792a43283
eduardorascon <eduardo.rascon@gmail.com>
2017-04-03 21:59:50 -0500

    Models changed to implement the one-to-many relationship between Client and CalfSale.

--- app/CalfSale.php
+++ app/CalfSale.php
@@ -10,6 +10,11 @@ class CalfSale extends Model
     protected $table = 'calves_sales';
     public $timestamps = false;
 
+    public function client()
+    {
+    	return $this->belongsTo('App\Client', 'client_id', 'id');
+    }
+
     public function getSaleDateAttribute($value)
     {
         setLocale(LC_TIME, 'es_MX.UTF-8', 'Spanish_Spain.1252');
--- app/Client.php
+++ app/Client.php
@@ -6,9 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model
 {
+	protected $table = 'clients';
 	public $timestamps = false;
 
     protected $fillable = [
         'first_name', 'last_name', 'address','company', 'phone'
     ];
+
+    public function purchases()
+    {
+    	return $this->hasMany('App\CalfSale');
+    }
 }
