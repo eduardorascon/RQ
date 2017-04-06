a9dae1d4ef0aa29e176bed4f42f2159c65a7047c
eduardorascon <eduardo.rascon@gmail.com>
2017-04-06 00:07:42 -0500

    Validations added to the Bull model.

--- app/Bull.php
+++ app/Bull.php
@@ -8,6 +8,10 @@ class Bull extends Model
 {
 	public $timestamps = false;
 
+	protected $fillable = [
+        'cattle_id'
+    ];
+
 	public function cattle()
 	{
 		return $this->hasOne('App\Cattle' , 'id', 'cattle_id');
