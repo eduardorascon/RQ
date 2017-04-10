cee945eff67e15cc0ae3433ef56ae5045718ae61
eduardorascon <eduardo.rascon@gmail.com>
2017-04-10 17:39:31 -0500

    Error fixed with the name of the request classes.

--- app/Http/Requests/StoreUpdateBreedRequest.php
+++ app/Http/Requests/StoreUpdateBreedRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreUpdateBreed extends FormRequest
+class StoreUpdateBreedRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
--- app/Http/Requests/StoreUpdateVaccineRequest.php
+++ app/Http/Requests/StoreUpdateVaccineRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Contracts\Validation\Validator;
 
-class StoreUpdateVaccine extends FormRequest
+class StoreUpdateVaccineRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
