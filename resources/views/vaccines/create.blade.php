8cbd2571bb1547f27885751ff7bb93511d11391b
eduardorascon <eduardo.rascon@gmail.com>
2017-04-14 13:09:11 -0500

    Validations added to the breed create and edit blades.

--- resources/views/vaccines/create.blade.php
+++ resources/views/vaccines/create.blade.php
@@ -22,10 +22,10 @@
                     <form class="form-horizontal" action="{{ route('vaccines.store') }}" method="post">
                         {{ csrf_field() }}
                         <div class="form-group">
-                          <label class="col-sm-2 control-label" for="name">Nombre:</label>
-                          <div class="col-sm-10">
-                            <input type="text" name="name" class="form-control" placeholder="Nombre" >
-                          </div>
+                            <label class="col-sm-2 control-label" for="name">Nombre:</label>
+                            <div class="col-sm-10">
+                                <input type="text" name="name" class="form-control" placeholder="Nombre" >
+                            </div>
                         </div>
 
                         <div class="form-group">
