4a780df5b298b37ff53fbd663b2db1cf1414e28c
eduardorascon <eduardo.rascon@gmail.com>
2017-04-16 20:49:22 -0500

    Validation messages added to the StoreUpdateCalfSaleRequest file.

--- app/Http/Requests/StoreUpdateCalfSaleRequest.php
+++ app/Http/Requests/StoreUpdateCalfSaleRequest.php
@@ -16,17 +16,23 @@ class StoreUpdateCalfSaleRequest extends FormRequest
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
     public function rules()
     {
         return [
             'sale_date'=> 'required',
             'sale_weight' => 'required',
             'price_per_kilo' => 'required',
-            'client_id' => 'required'];
+            'client_id' => 'required'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'sale_date.required' => 'La fecha de venta es necesaria para el registro.',
+            'sale_weight.required' => 'El peso de venta es necesaria para el registro.',
+            'price_per_kilo.required' => 'El precio por kilo es necesaria para el registro.',
+            'client_id.required' => 'El cliente es necesario para el registro.'
+        ];
     }
 }
