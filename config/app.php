b4c9c5f15d7ec39339cc1e5685c7c13e6c47e8b4
eduardorascon <eduardo.rascon@gmail.com>
2017-04-19 22:44:55 -0500

    Provider and alias added.

--- config/app.php
+++ config/app.php
@@ -162,7 +162,10 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
+        /*
+         * Lavacharts
+         */
+        Khill\Lavacharts\Laravel\LavachartsServiceProvider::class,
         /*
          * Package Service Providers...
          */
@@ -209,6 +212,7 @@ return [
         'Gate' => Illuminate\Support\Facades\Gate::class,
         'Hash' => Illuminate\Support\Facades\Hash::class,
         'Lang' => Illuminate\Support\Facades\Lang::class,
+        'Lava' => Khill\Lavacharts\Laravel\LavachartsFacade::class,
         'Log' => Illuminate\Support\Facades\Log::class,
         'Mail' => Illuminate\Support\Facades\Mail::class,
         'Notification' => Illuminate\Support\Facades\Notification::class,
