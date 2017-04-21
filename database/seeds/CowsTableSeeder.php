95da57824ac06c0190c2212a4100712d11bace7c
eduardorascon <eduardo.rascon@gmail.com>
2017-04-21 17:06:59 -0500

    Table seeder updated.

--- database/seeds/CowsTableSeeder.php
+++ database/seeds/CowsTableSeeder.php
@@ -15,14 +15,17 @@ class CowsTableSeeder extends Seeder
 
         DB::table('cows')->insert([
             'is_fertile'=> 'Si',
+            'pregnancy_status'=> 'Preñada',
 			'cattle_id'=> 3]);
 
         DB::table('cows')->insert([
             'is_fertile'=> 'No',
+            'pregnancy_status'=> 'Parida',
             'cattle_id'=> 4]);
 
         DB::table('cows')->insert([
             'is_fertile'=> 'Si',
+            'pregnancy_status'=> 'Vacia',
             'cattle_id'=> 5]);
     }
 }
