207ea2a61b8fdcb0148f4b33cf01aea8c26452e9
eduardorascon <eduardo.rascon@gmail.com>
2017-04-21 17:06:45 -0500

    Database fields pregnancy_statuts and last_pregnancy_check added.

--- database/migrations/2017_03_12_235258_create_cows_table.php
+++ database/migrations/2017_03_12_235258_create_cows_table.php
@@ -6,27 +6,20 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateCowsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+
     public function up()
     {
         Schema::create('cows', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
             $table->string('is_fertile');
+            $table->string('pregnancy_status');
+            $table->date('last_pregnancy_check');
             $table->integer('cattle_id')->unsigned();
             $table->foreign('cattle_id')->references('id')->on('cattle');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         //
