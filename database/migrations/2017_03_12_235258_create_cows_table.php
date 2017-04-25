ae613d5d7bdc0f8e74cc02dc656c226ed28ffb20
eduardorascon <eduardo.rascon@gmail.com>
2017-04-25 13:34:25 -0500

    Field 'last_pregnancy_check' marked as nullable.

--- database/migrations/2017_03_12_235258_create_cows_table.php
+++ database/migrations/2017_03_12_235258_create_cows_table.php
@@ -14,7 +14,7 @@ class CreateCowsTable extends Migration
             $table->timestamps();
             $table->string('is_fertile');
             $table->string('pregnancy_status');
-            $table->date('last_pregnancy_check');
+            $table->date('last_pregnancy_check')->nullable();
             $table->integer('cattle_id')->unsigned();
             $table->foreign('cattle_id')->references('id')->on('cattle');
         });
