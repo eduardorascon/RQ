348ee31c92aab215ca47b10f2b9047a5baef76dc
eduardorascon <eduardo.rascon@gmail.com>
2017-05-05 11:26:55 -0500

    CowsTableSeeder updated.

--- database/seeds/CowsTableSeeder.php
+++ database/seeds/CowsTableSeeder.php
@@ -7,24 +7,9 @@ class CowsTableSeeder extends Seeder
 
     public function run()
     {
-        $now = date('Y-m-d H:i:s', strtotime('now'));
-
-        DB::table('cows')->insert([
-            'is_fertile'=> 'Si',
-            'pregnancy_status'=> 'Preñada',
-            'number_of_calves'=> 0,
-			'cattle_id'=> 3]);
-
-        DB::table('cows')->insert([
-            'is_fertile'=> 'Si',
-            'pregnancy_status'=> 'Parida',
-            'number_of_calves'=> 0,
-            'cattle_id'=> 4]);
-
-        DB::table('cows')->insert([
-            'is_fertile'=> 'No',
-            'pregnancy_status'=> 'Vacia',
-            'number_of_calves'=> 0,
-            'cattle_id'=> 5]);
+        Eloquent::unguard();
+        DB::disableQueryLog();
+        $path = 'database/seeds/sql/cows.sql';
+        DB::unprepared(file_get_contents($path));
     }
 }
