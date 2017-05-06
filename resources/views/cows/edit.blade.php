147ec7a783ffa3ea182ad12d5c77daf22e2318cb
eduardorascon <eduardo.rascon@gmail.com>
2017-05-06 10:50:07 -0500

    Small layout changes.

--- resources/views/cows/edit.blade.php
+++ resources/views/cows/edit.blade.php
@@ -24,29 +24,29 @@
 						{{ csrf_field() }}
 
 						<div class="form-group">
-						<label class="col-sm-2 control-label" for="cattle_tag">Arete Siniga</label>
-						<div class="col-sm-10">
+						<label class="col-sm-3 control-label" for="cattle_tag">Arete Siniga</label>
+						<div class="col-sm-9">
 							<input type="text" name="cattle_tag" class="form-control" placeholder="Etiqueta" value="{{ $cow->cattle->tag }}" />
 						</div>
 						</div>
 
 						<div class="form-group">
-						<label class="col-sm-2 control-label" for="cattle_birth_date">Fecha de nacimiento</label>
-						<div class="col-sm-10">
+						<label class="col-sm-3 control-label" for="cattle_birth_date">Fecha de nacimiento</label>
+						<div class="col-sm-9">
 							<input type="date" name="cattle_birth_date" class="form-control" placeholder="Fecha de nacimiento" value="{{ $cow->cattle->birth }}" />
 						</div>
 						</div>
 
 						<div class="form-group">
-						<label class="col-sm-2 control-label" for="cattle_purchase_date">Fecha de compra</label>
-						<div class="col-sm-10">
+						<label class="col-sm-3 control-label" for="cattle_purchase_date">Fecha de compra</label>
+						<div class="col-sm-9">
 							<input type="date" name="cattle_purchase_date" class="form-control" placeholder="Fecha de compra" value="{{ $cow->cattle->purchase_date }}" />
 						</div>
 						</div>
 
 						<div class="form-group">
-						<label class="col-sm-2 control-label" for="cattle_breed">Raza</label>
-						<div class="col-sm-10">
+						<label class="col-sm-3 control-label" for="cattle_breed">Raza</label>
+						<div class="col-sm-9">
 							<select class="form-control" name="cattle_breed">
 								@foreach ($breed_list as $b)
 								{
@@ -62,8 +62,8 @@
 						</div>
 
 						<div class="form-group">
-						<label class="col-sm-2 control-label" for="cow_fertility">¿Es fertil?</label>
-						<div class="col-sm-10">
+						<label class="col-sm-3 control-label" for="cow_fertility">¿Es fertil?</label>
+						<div class="col-sm-9">
 							<select class="form-control" name="cow_fertility">
 								@if($cow->is_fertile == 'Si')
 									<option value="Si" selected="selected">Si</option>
@@ -77,8 +77,8 @@
 						</div>
 
 						<div class="form-group">
-						<label class="col-sm-2 control-label" for="cow_pregnancy_status">Estatus de gestación</label>
-						<div class="col-sm-10">
+						<label class="col-sm-3 control-label" for="cow_pregnancy_status">Estatus de gestación</label>
+						<div class="col-sm-9">
 							<select class="form-control" name="cow_pregnancy_status">
 								@if($cow->pregnancy_status == 'Vacia')
 									<option value="Vacia" selected="selected">Vacia</option>
@@ -98,15 +98,15 @@
 						</div>
 
 						<div class="form-group">
-						<label class="col-sm-2 control-label" for="cow_last_pregnancy_check">Fecha de revision</label>
-						<div class="col-sm-10">
+						<label class="col-sm-3 control-label" for="cow_last_pregnancy_check">Fecha de revision</label>
+						<div class="col-sm-9">
 							<input type="date" name="cow_last_pregnancy_check" class="form-control" value="{{ $cow->last_pregnancy_checked_date }}" />
 						</div>
 						</div>
 
 						<div class="form-group">
-						<label class="col-sm-2 control-label" for="cattle_is_alive">¿Esta viva?</label>
-						<div class="col-sm-10">
+						<label class="col-sm-3 control-label" for="cattle_is_alive">¿Esta viva?</label>
+						<div class="col-sm-9">
 							<select class="form-control" name="cattle_is_alive">
 								<option value="">Elige la opcion.</option>
 								<option value="Si">Si</option>
@@ -116,8 +116,8 @@
 						</div>
 
 						<div class="form-group">
-						<label class="col-sm-2 control-label" for="cow_number_of_calves">Numero de becerros</label>
-						<div class="col-sm-10">
+						<label class="col-sm-3 control-label" for="cow_number_of_calves">Numero de becerros</label>
+						<div class="col-sm-9">
 							<input type="number" name="cow_number_of_calves" class="form-control" placeholder="Numero de becerros" value="0">
 						</div>
 						</div>
