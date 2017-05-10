c5b1608fcf54936394fa04f90541c54624e46dd6
eduardorascon <eduardo.rascon@gmail.com>
2017-05-10 12:59:29 -0500

    Search by breed added.

--- app/Http/Controllers/CowFilterController.php
+++ app/Http/Controllers/CowFilterController.php
@@ -16,13 +16,18 @@ class CowFilterController extends Controller
                 orderBy('cattle.tag', 'asc');
         else
         {
-            $cows = (new Cow)->newQuery();
+            $cows = (new Cow)->newQuery()->
+                join('cattle', 'cows.cattle_id', '=', 'cattle.id');
 
             //search by cattle tag
             if($request->has('cattle_tag'))
             {
-                $cows->join('cattle', 'cows.cattle_id', '=', 'cattle.id')->
-                    where('cattle.tag', $request->cattle_tag);
+                $cows->where('cattle.tag', $request->cattle_tag);
+            }
+
+            if($request->has('cattle_breed'))
+            {
+                $cows->where('cattle.breed_id', $request->cattle_breed);
             }
         }
 
