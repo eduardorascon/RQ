b2a093573c2e38492bf82a924d2755852219819d
eduardorascon <eduardo.rascon@gmail.com>
2017-05-20 23:29:25 -0500

    Migration files updated.

--- database/migrations/2017_05_18_235129_create_owners_table.php
+++ database/migrations/2017_05_18_235129_create_owners_table.php
@@ -6,23 +6,17 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateOwnersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
-        //
+        Schema::create('owners', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+        });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
-        //
+        Schema::dropIfExists('owners');
     }
 }
