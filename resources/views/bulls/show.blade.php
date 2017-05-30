e73500cd9ef08f79f08b288dd47e500b638b91d4
eduardorascon <eduardo.rascon@gmail.com>
2017-05-30 12:11:54 -0500

    HTML changed to make room for charts.

--- resources/views/bulls/show.blade.php
+++ resources/views/bulls/show.blade.php
@@ -122,27 +122,35 @@
                     </div>
 				@endif
 				<div class="panel-body">
-					<form class="form-inline" action="{{ route('bull_log_weight', $bull->id) }}" method="post">
+					<form class="form-horizontal" action="{{ route('bull_log_weight', $bull->id) }}" method="post">
 						{{ csrf_field() }}
 						<div class="row">
-							<div class="col-sm-10">
-								<div class="form-group col-sm-5">
-								<label class="col-sm-2" for="weight">Peso</label>
-								<input type="number" step="any" class="form-control col-sm-offset-2 col-sm-2" name="weight" id="weight" placeholder="Peso">
+							<div class="col-sm-4">
+
+								<div class="form-group">
+								<label class="col-sm-3 control-label" for="weight">Peso</label>
+								<div class="col-sm-9">
+									<input type="number" step="any" class="form-control" name="weight" id="weight" placeholder="Peso">
+								</div>
 								</div>
 
-								<div class="form-group col-sm-5">
-								<label class="col-sm-2" for="date">Fecha</label>
-								<input type="date" class="form-control col-sm-offset-2 col-sm-2" name="date" id="date" placeholder="Fecha">
+								<div class="form-group">
+								<label class="col-sm-3 control-label" for="date">Fecha</label>
+								<div class="col-sm-9">
+									<input type="date" class="form-control" name="date" id="date" placeholder="Fecha">
+								</div>
 								</div>
 
-								<div class="form-group col-sm-5">
-								<label class="col-sm-2" for="comment">Comentario</label>
-								<input type="text" class="form-control col-sm-offset-2 col-sm-2" name="comment" id="comment" placeholder="Comentario">
+								<div class="form-group">
+								<label class="col-sm-4 control-label" for="comment">Comentario</label>
+								<div class="col-sm-9">
+									<input type="text" class="form-control" name="comment" id="comment" placeholder="Comentario">
 								</div>
+								</div>
+								<button type="submit" class="btn btn-primary">Guardar</button>
 							</div>
-							<div class="col-sm-2">
-								<button type="submit" class="btn btn-default">Guardar</button>
+							<div class="col-sm-8">
+
 							</div>
 						</div>
 					</form>
