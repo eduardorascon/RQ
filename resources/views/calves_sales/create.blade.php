f359d2ebe3f8b65d7fd44e88d5375693c940e38d
eduardorascon <eduardo.rascon@gmail.com>
2017-06-08 18:56:24 -0500

    Small bootstrap changes.

--- resources/views/calves_sales/create.blade.php
+++ resources/views/calves_sales/create.blade.php
@@ -23,8 +23,8 @@
 					{{ csrf_field() }}
 
 					<div class="form-group">
-					<label class="col-sm-2 control-label" for="client_id">Cliente</label>
-					<div class="col-sm-10">
+					<label class="col-sm-3 control-label" for="client_id">Cliente</label>
+					<div class="col-sm-9">
 						<select class="form-control" name="client_id">
 							@foreach ($client_list as $client)
 							{
@@ -36,36 +36,36 @@
 					</div>
 
 					<div class="form-group">
-					<label class="col-sm-2 control-label" for="calf_tag">Arete Siniga</label>
-					<div class="col-sm-10">
+					<label class="col-sm-3 control-label" for="calf_tag">Arete Siniga</label>
+					<div class="col-sm-9">
 						<input type="text" name="calf_tag" class="form-control" readonly="readonly" value="{{ $calf->cattle->tag }}">
 					</div>
 					</div>
 
 					<div class="form-group">
-					<label class="col-sm-2 control-label" for="calf_tag">Raza</label>
-					<div class="col-sm-10">
+					<label class="col-sm-3 control-label" for="calf_tag">Raza</label>
+					<div class="col-sm-9">
 						<input type="text" name="calf_breed" class="form-control" readonly="readonly" value="{{ $calf->cattle->breed->name }}">
 					</div>
 					</div>
 
 					<div class="form-group">
-					<label class="col-sm-2 control-label" for="sale_date">Fecha de venta</label>
-					<div class="col-sm-10">
+					<label class="col-sm-3 control-label" for="sale_date">Fecha de venta</label>
+					<div class="col-sm-9">
 						<input type="date" name="sale_date" class="form-control" placeholder="Fecha...">
 					</div>
 					</div>
 
 					<div class="form-group">
-					<label class="col-sm-2 control-label" for="sale_weight">Peso de venta</label>
-					<div class="col-sm-10">
+					<label class="col-sm-3 control-label" for="sale_weight">Peso de venta</label>
+					<div class="col-sm-9">
 						<input type="number" step="any" name="sale_weight" class="form-control" placeholder="Peso...">
 					</div>
 					</div>
 
 					<div class="form-group">
-					<label class="col-sm-2 control-label" for="price_per_kilo">Precio por kilo</label>
-					<div class="col-sm-10">
+					<label class="col-sm-3 control-label" for="price_per_kilo">Precio por kilo</label>
+					<div class="col-sm-9">
 						<div class="input-group">
 						<div class="input-group-addon">$</div>
 						<input type="number" step="any" name="price_per_kilo" class="form-control" placeholder="Precio por kilo...">
