2243276f38ecec70d1377a01aca627d83264abf2
eduardorascon <eduardo.rascon@gmail.com>
2017-06-19 12:48:37 -0500

    Calves purchase date can be set to null.

--- app/Http/Requests/StoreUpdateCalfRequest.php
+++ app/Http/Requests/StoreUpdateCalfRequest.php
@@ -15,7 +15,6 @@ class StoreUpdateCalfRequest extends FormRequest
     {
         return [
             'cattle_birth_date' => 'required',
-            'cattle_purchase_date' => 'required',
             'cattle_breed' => 'required',
             'cattle_gender' => 'required',
             'cattle_is_alive' => 'required'
@@ -26,7 +25,6 @@ class StoreUpdateCalfRequest extends FormRequest
     {
         return [
             'cattle_birth_date.required' => 'La fecha de nacimiento es necesaria para el registro.',
-            'cattle_purchase_date.required' => 'La fecha de compra es necesaria para el registro.',
             'cattle_breed.required' => 'La raza es necesaria para el registro.',
             'cattle_gender.required' => 'El sexo es necesario para el registro.',
             'cattle_is_alive.required' => 'El estatus ¿Esta vivo? es necesario para el registro.'
