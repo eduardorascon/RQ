fff76321bfbfdcf50db6b8c5608c9e59c2283026
eduardorascon <eduardo.rascon@gmail.com>
2017-06-19 13:13:26 -0500

    More validation messages added.

--- app/Http/Requests/StoreUpdateCalfRequest.php
+++ app/Http/Requests/StoreUpdateCalfRequest.php
@@ -17,8 +17,10 @@ class StoreUpdateCalfRequest extends FormRequest
             'cow_id' => 'required',
             'cattle_birth_date' => 'required',
             'cattle_breed' => 'required',
-            'cattle_gender' => 'required',
-            'cattle_is_alive' => 'required'
+            'cattle_owner' => 'required',
+            'cattle_paddock' => 'required',
+            'cattle_is_alive' => 'required',
+            'cattle_gender' => 'required'
         ];
     }
 
@@ -28,8 +30,10 @@ class StoreUpdateCalfRequest extends FormRequest
             'cow_id.required' => 'La madre del becerro es necesaria para el registro.',
             'cattle_birth_date.required' => 'La fecha de nacimiento es necesaria para el registro.',
             'cattle_breed.required' => 'La raza es necesaria para el registro.',
-            'cattle_gender.required' => 'El sexo es necesario para el registro.',
-            'cattle_is_alive.required' => 'El estatus ¿Esta vivo? es necesario para el registro.'
+            'cattle_owner.required' => 'El dueño del becerro es necesario para el registro.',
+            'cattle_paddock.required' => 'El potrero del becerro es necesario para el registro.',
+            'cattle_is_alive.required' => 'El estatus ¿Esta vivo? es necesario para el registro.',
+            'cattle_gender.required' => 'El sexo es necesario para el registro.'
         ];
     }
 }
