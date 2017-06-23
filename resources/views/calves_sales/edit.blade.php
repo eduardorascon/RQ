0497fdb8b627cc14bf1612c4768c98dfac96f5c2
eduardorascon <eduardo.rascon@gmail.com>
2017-06-23 12:06:22 -0500

    Calves sales layouts changed.

--- resources/views/calves_sales/edit.blade.php
+++ resources/views/calves_sales/edit.blade.php
@@ -79,7 +79,7 @@
 					</div>
 
 					<div class="form-group">
-					<div class="col-sm-offset-2 col-sm-10">
+					<div class="col-sm-offset-3 col-sm-9">
 						<input type="submit" class="btn btn-primary" value="Guardar">
 					</div>
 					</div>
