f1d3be5f9678a7b25f304a49a7dbbe63acd90bb7
eduardorascon <eduardo.rascon@gmail.com>
2017-07-02 18:48:38 -0500

    Controllers updated to handle new dates.

--- app/Http/Controllers/CalfController.php
+++ app/Http/Controllers/CalfController.php
@@ -69,8 +69,8 @@ class CalfController extends Controller
 
         $cattle = new Cattle;
         $cattle->tag = $request->cattle_tag;
-        $cattle->birth = $request->cattle_birth_date;
-        $cattle->purchase_date = $request->cattle_purchase_date;
+        $cattle->birth = Carbon::createFromFormat('d/m/Y', $request->cattle_birth_date);
+        $cattle->purchase_date = Carbon::createFromFormat('d/m/Y', $request->cattle_purchase_date);
         $cattle->breed_id = $request->cattle_breed;
         $cattle->owner_id = $request->cattle_owner;
         $cattle->paddock_id = $request->cattle_paddock;
@@ -120,8 +120,8 @@ class CalfController extends Controller
         $calf = Calf::findOrFail($id);
         $cattle = $calf->cattle;
         $cattle->tag = $request->cattle_tag;
-        $cattle->birth = $request->cattle_birth_date;
-        $cattle->purchase_date = $request->cattle_purchase_date;
+        $cattle->birth = Carbon::createFromFormat('d/m/Y', $request->cattle_birth_date);
+        $cattle->purchase_date = Carbon::createFromFormat('d/m/Y', $request->cattle_purchase_date);
         $cattle->breed_id = $request->cattle_breed;
         $cattle->owner_id = $request->cattle_owner;
         $cattle->paddock_id = $request->cattle_paddock;
