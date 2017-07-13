da9d5903bef8ec631776ad4951552dace8890e22
eduardorascon <eduardo.rascon@gmail.com>
2017-07-13 12:32:47 -0500

    More layout changes.

--- resources/views/bulls/index.blade.php
+++ resources/views/bulls/index.blade.php
@@ -6,8 +6,8 @@
 		<div class="col-md-offset-1 col-md-10">
 			<div class="panel panel-default">
 				<div class="panel-heading">
-					<strong>Total de Registros: {{ $total_bulls }}</strong>
-						<a class="btn btn-primary btn-sm" data-container="body" data-toggle="tooltip" data-placement="top" title="Agregar nuevo registro" href="{{ route('bulls.create') }}">
+					<strong>TOROS, Total de Registros: {{ $total_bulls }}</strong>
+					<a class="btn btn-info btn-sm" data-container="body" data-toggle="tooltip" data-placement="top" title="Agregar nuevo registro" href="{{ route('bulls.create') }}">
 						<span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
 					</a>
 				</div>
