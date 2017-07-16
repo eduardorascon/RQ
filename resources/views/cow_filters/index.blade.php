cefcc3be60eed1fa5019de9fe35b664de29f3c5f
eduardorascon <eduardo.rascon@gmail.com>
2017-07-16 15:32:56 -0500

    More layout improvements.

--- resources/views/cow_filters/index.blade.php
+++ resources/views/cow_filters/index.blade.php
@@ -12,10 +12,22 @@
 					<form class="form-horizontal" action="{{ route('cow_filters.index') }}" method="get">
 
 						<div class="form-group">
-						<label class="col-sm-3 control-label" for="cattle_tag">Arete Siniga</label>
-						<div class="col-sm-9">
-							<input type="text" name="cattle_tag" class="form-control" placeholder="Etiqueta">
-						</div>
+							<label class="control-label col-sm-3" for="cattle_tag">Arete Siniga</label>
+							<div class="col-sm-4">
+								<input type="text" name="cattle_tag" class="form-control" placeholder="Etiqueta">
+							</div>
+
+							<label class="control-label col-sm-1" for="cattle_breed">Raza</label>
+							<div class="col-sm-4">
+								<select class="form-control" name="cattle_breed">
+									<option value="">Todas las opciones</option>
+									@foreach ($breed_list as $b)
+									{
+									<option value="{{ $b->id }}">{{ $b->name }}</option>
+									}
+									@endforeach
+								</select>
+							</div>
 						</div>
 
 						<div class="form-group">
@@ -55,95 +67,75 @@
 						</div>
 
 						<div class="form-group">
-						<label class="col-sm-3 control-label" for="cattle_breed">Raza</label>
-						<div class="col-sm-9">
-							<select class="form-control" name="cattle_breed">
-								<option value="">Todas las opciones</option>
-								@foreach ($breed_list as $b)
-								{
-								<option value="{{ $b->id }}">{{ $b->name }}</option>
-								}
-								@endforeach
-							</select>
-						</div>
+							<label class="col-sm-3 control-label" for="cattle_owner">Dueño</label>
+							<div class="col-sm-4">
+								<select class="form-control" name="cattle_owner">
+									<option value="">Todas las opciones</option>
+									@foreach ($owner_list as $o)
+									{
+									<option value="{{ $o->id }}">{{ $o->name }}</option>
+									}
+									@endforeach
+								</select>
+							</div>
+
+							<label class="col-sm-1 control-label" for="cattle_paddock">Potrero</label>
+							<div class="col-sm-4">
+								<select class="form-control" name="cattle_paddock">
+									<option value="">Todas las opciones</option>
+									@foreach ($paddock_list as $p)
+									{
+									<option value="{{ $p->id }}">{{ $p->name }}</option>
+									}
+									@endforeach
+								</select>
+							</div>
 						</div>
 
 						<div class="form-group">
-						<label class="col-sm-3 control-label" for="cattle_owner">Dueño</label>
-						<div class="col-sm-9">
-							<select class="form-control" name="cattle_owner">
-								<option value="">Todas las opciones</option>
-								@foreach ($owner_list as $o)
-								{
-								<option value="{{ $o->id }}">{{ $o->name }}</option>
-								}
-								@endforeach
-							</select>
-						</div>
+							<label class="control-label col-sm-3" for="cow_fertility">Fertilidad</label>
+							<div class="col-sm-3">
+								<select class="form-control" name="cow_fertility">
+									<option value="">Todas las opciones</option>
+									<option value="Si">Si</option>
+									<option value="No">No</option>
+								</select>
+							</div>
+
+							<label class="control-label col-sm-3" for="cow_pregnancy_status">Estado de gestación</label>
+							<div class="col-sm-3">
+								<select class="form-control" name="cow_pregnancy_status">
+									<option value="">Todas las opciones</option>
+									<option value="Vacia">Vacia</option>
+									<option value="Preñada">Preñada</option>
+									<option value="Parida">Parida</option>
+								</select>
+							</div>
 						</div>
 
 						<div class="form-group">
-						<label class="col-sm-3 control-label" for="cattle_paddock">Potrero</label>
-						<div class="col-sm-9">
-							<select class="form-control" name="cattle_paddock">
-								<option value="">Todas las opciones</option>
-								@foreach ($paddock_list as $p)
-								{
-								<option value="{{ $p->id }}">{{ $p->name }}</option>
-								}
-								@endforeach
-							</select>
-						</div>
-						</div>
+							<label class="control-label col-sm-3" for="cattle_is_alive">¿Esta viva?</label>
+							<div class="col-sm-3">
+								<select class="form-control" name="cattle_is_alive">
+									<option value="">Todas las opciones</option>
+									<option value="Si">Si</option>
+									<option value="No">No</option>
+								</select>
+							</div>
 
-						<div class="form-group">
-						<label class="col-sm-3 control-label" for="cow_fertility">Fertilidad</label>
-						<div class="col-sm-9">
-							<select class="form-control" name="cow_fertility">
-								<option value="">Todas las opciones</option>
-								<option value="Si">Si</option>
-								<option value="No">No</option>
-							</select>
-						</div>
-						</div>
-
-						<div class="form-group">
-						<label class="col-sm-3 control-label" for="cow_pregnancy_status">Estado de gestación</label>
-						<div class="col-sm-9">
-							<select class="form-control" name="cow_pregnancy_status">
-								<option value="">Todas las opciones</option>
-								<option value="Vacia">Vacia</option>
-								<option value="Preñada">Preñada</option>
-								<option value="Parida">Parida</option>
-							</select>
-						</div>
-						</div>
-
-						<div class="form-group">
-						<label class="col-sm-3 control-label" for="cattle_is_alive">¿Esta viva?</label>
-						<div class="col-sm-9">
-							<select class="form-control" name="cattle_is_alive">
-								<option value="">Todas las opciones</option>
-								<option value="Si">Si</option>
-								<option value="No">No</option>
-							</select>
-						</div>
-						</div>
-
-						<div class="form-group">
-						<label class="col-sm-3 control-label" for="cow_currently_sold">¿Fue vendida?</label>
-						<div class="col-sm-9">
-							<select class="form-control" name="cow_currently_sold">
-								<option value="">Todas las opciones</option>
-								<option value="Si">Si</option>
-								<option value="No">No</option>
-							</select>
-						</div>
+							<label class="control-label col-sm-3" for="cow_currently_sold">¿Fue vendida?</label>
+							<div class="col-sm-3">
+								<select class="form-control" name="cow_currently_sold">
+									<option value="">Todas las opciones</option>
+									<option value="Si">Si</option>
+									<option value="No">No</option>
+								</select>
+							</div>
 						</div>
 
 						<div class="form-group">
 						<label class="col-sm-3 control-label" for="cow_number_of_calves">Número de becerros</label>
-						<div class="col-sm-9">
+						<div class="col-sm-3">
 							<input type="number" name="cow_number_of_calves" class="form-control" placeholder="Numero de becerros" value="0">
 						</div>
 						</div>
