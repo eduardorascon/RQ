90b8693b0b394eaed2c63a5c8925c9e7a1af7461
eduardorascon <eduardo.rascon@gmail.com>
2017-07-18 11:58:36 -0500

    HTML improved for index views.

--- resources/views/breeds/index.blade.php
+++ resources/views/breeds/index.blade.php
@@ -5,31 +5,41 @@
     <div class="row">
         <div class="col-md-offset-1 col-md-10">
             <div class="panel panel-default">
-                <div class="panel-heading">Razas ({{ count($breeds) }}), <a href="{{ route('breeds.create') }}">Agregar nueva raza</a></div>
+                <div class="panel-heading">
+                    <strong>RAZAS, registros {{ count($breeds) }}, </strong>
+                    <a href="{{ route('breeds.create') }}">Agregar nuevo registro</a>
+                </div>
                 <div class="panel-body">
-                    <table class="table table-striped">
-                	<thead>
-                		<tr>
-                			<th>Nombre</th>
-                			<th></th>
-                		</tr>
-                	</thead>
-                	<tbody>
-                	@foreach($breeds as $breed)
-                		<tr>
-                			<td>{{ $breed->name }}</td>
-                			<td>
-                				<form class="" action="{{route('breeds.destroy', $breed->id)}}" method="post">
-                                    <input type="hidden" name="_method" value="delete" />
-                					<input type="hidden" name="_token" value="{{ csrf_token() }}" />
-                					<a href="{{route('breeds.edit',$breed->id)}}" class="btn btn-primary btn-xs">Editar</a>
-                                    <input class="btn btn-danger btn-xs" type="submit" onclick="return confirm('El registro será eliminado');" name="btnBorrar" value="Eliminar" />
-                				</form>
-                			</td>
-                		</tr>
-        			@endforeach
-                	</tbody>
-                    </table>
+                    <div class="table-responsive">
+                        <table class="table table-hover table-condensed">
+                    	<thead>
+                    		<tr>
+                    			<th>Nombre</th>
+                    			<th>Acciones</th>
+                    		</tr>
+                    	</thead>
+                    	<tbody>
+                    	@foreach($breeds as $breed)
+                    		<tr>
+                    			<td>{{ $breed->name }}</td>
+                    			<td>
+                    				<form class="" action="{{route('breeds.destroy', $breed->id)}}" method="post">
+                                        <input type="hidden" name="_method" value="delete" />
+                    					<input type="hidden" name="_token" value="{{ csrf_token() }}" />
+
+                                        <a href="{{route('breeds.edit',$breed->id)}}" class="btn btn-warning btn-sm" data-container="body" data-toggle="tooltip" data-placement="top" title="Editar información del registro">
+                                            <span class="glyphicon glyphicon-open" aria-hidden="true"></span>
+                                        </a>
+                                        <button class="btn btn-danger btn-sm" type="submit" onclick="return confirm('El registro será eliminado');" name="btnBorrar" value="Eliminar" data-container="body" data-toggle="tooltip" data-placement="top" title="Eliminar el registro">
+                                            <span class="glyphicon glyphicon-alert" aria-hidden="true"></span>
+                                        </button>
+                    				</form>
+                    			</td>
+                    		</tr>
+            			@endforeach
+                    	</tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/vaccines/index.blade.php
+++ resources/views/vaccines/index.blade.php
@@ -5,32 +5,41 @@
     <div class="row">
         <div class="col-md-offset-1 col-md-10">
             <div class="panel panel-default">
-                <div class="panel-heading">Vacunas ({{ count($vaccines) }}), <a href="{{ route('vaccines.create') }}">Agregar nueva vacuna</a>
+                <div class="panel-heading">
+                    <strong>VACUNAS, registros: {{ count($vaccines) }}, </strong>
+                    <a href="{{ route('vaccines.create') }}">Agregar nuevo registro</a>
                 </div>
                 <div class="panel-body">
-                    <table class="table table-striped">
-                	<thead>
-                		<tr>
-                			<th>Nombre</th>
-                			<th></th>
-                		</tr>
-                	</thead>
-                	<tbody>
-                	@foreach($vaccines as $vaccine)
-                		<tr>
-                			<td>{{ $vaccine->name }}</td>
-                			<td>
-                				<form class="" action="{{route('vaccines.destroy', $vaccine->id)}}" method="post">
-                                    <input type="hidden" name="_method" value="delete">
-                					<input type="hidden" name="_token" value="{{ csrf_token() }}">
-                					<a href="{{route('vaccines.edit', $vaccine->id)}}" class="btn btn-primary btn-xs">Editar</a>
-                                    <input class="btn btn-danger btn-xs" type="submit" onclick="return confirm('El registro será eliminado');" name="btnBorrar" value="Eliminar" />
-                				</form>
-                			</td>
-                		</tr>
-        			@endforeach
-                	</tbody>
-                    </table>
+                    <div class="table-responsive">
+                        <table class="table table-hover table-condensed">
+                    	<thead>
+                    		<tr>
+                    			<th>Nombre</th>
+                    			<th>Acciones</th>
+                    		</tr>
+                    	</thead>
+                    	<tbody>
+                    	@foreach($vaccines as $vaccine)
+                    		<tr>
+                    			<td>{{ $vaccine->name }}</td>
+                    			<td>
+                    				<form class="" action="{{ route('vaccines.destroy', $vaccine->id) }}" method="post">
+                                        <input type="hidden" name="_method" value="delete">
+                    					<input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+                    					<a href="{{ route('vaccines.edit', $vaccine->id) }}" class="btn btn-warning btn-sm" data-container="body" data-toggle="tooltip" data-placement="top" title="Editar información del registro">
+                                            <span class="glyphicon glyphicon-open" aria-hidden="true"></span>
+                                        </a>
+                                        <button class="btn btn-danger btn-sm" type="submit" onclick="return confirm('El registro será eliminado');" name="btnBorrar" value="Eliminar" data-container="body" data-toggle="tooltip" data-placement="top" title="Eliminar el registro">
+                                            <span class="glyphicon glyphicon-alert" aria-hidden="true"></span>
+                                        </button>
+                    				</form>
+                    			</td>
+                    		</tr>
+            			@endforeach
+                    	</tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
