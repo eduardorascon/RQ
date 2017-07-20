106c7fd9471e8fec3f3bad2f953f5a6604086e0c
eduardorascon <eduardo.rascon@gmail.com>
2017-07-20 12:27:06 -0500

    HTML layout improvements.

--- resources/views/cows_sales/index.blade.php
+++ resources/views/cows_sales/index.blade.php
@@ -13,7 +13,7 @@
 						{{ csrf_field() }}
 
 						<div class="form-group">
-						<label class="control-label col-sm-3" for="search">Buscar arete siniga</label>
+						<label class="control-label col-sm-3" for="search">Arete Siniga:</label>
 						<div class="col-sm-9">
 							<div class="input-group col-sm-6">
 								<input type="text" class="form-control" name="search" placeholder="Buscar">
@@ -25,57 +25,58 @@
 							</div>
 						</div>
 						</div>
+
 					</form>
 				</div>
 				<div class="panel-body">
-						@if($cows->count() > 0)
-						<div class="table-responsive">
-							<table class="table table-hover table-condensed">
-							<thead>
-								<tr>
-									<th>Arete Siniga</th>
-									<th>Raza</th>
-									<th>Fecha de nacimiento</th>
-									<th>Fecha de compra</th>
-									<th>Fecha de venta</th>
-									<th>Acciones</th>
-								</tr>
-							</thead>
-							<tbody>
-							@foreach($cows as $cow)
-								<tr>
-									<td>{{ $cow->cattle->tag }}</td>
-									<td>{{ $cow->cattle->breed->name }}</td>
-									<td>{{ $cow->cattle->getBirthWithFormat() }}</td>
-									<td>{{ $cow->cattle->getPurchaseDateWithFormat() }}</td>
-									<td>
-										@if(count($cow->sale))
-										{{ $cow->sale->getSaleDateWithFormat() }}
-										@endif
-									</td>
-									<td>
-										@if(count($cow->sale) == 0)
-										<a class="btn btn-success btn-sm" href="{{ route('cows_sales.create', 'cow=' . $cow->id) }}" data-container="body" data-toggle="tooltip" data-placement="top" title="Crear registro de venta">
-											<span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
-										</a>
-										@else
-										<a class="btn btn-info btn-sm" href="{{ route('cows_sales.show', $cow->id) }}" data-container="body" data-toggle="tooltip" data-placement="top" title="Mostrar información del registro">
-											<span class="glyphicon glyphicon-file" aria-hidden="true"></span>
-										</a>
-										<a class="btn btn-warning btn-sm" href="{{ route('cows_sales.edit', $cow->id) }}" data-container="body" data-toggle="tooltip" data-placement="top" title="Editar información del registro">
-											<span class="glyphicon glyphicon-open" aria-hidden="true"></span>
-										</a>
-										@endif
-									</td>
-								</tr>
-							@endforeach
-							</tbody>
-							</table>
-						</div>
-						<div>{{ $cows->links() }}</div>
-						@endif
+				@if($cows->count() > 0)
+					<div class="table-responsive">
+						<table class="table table-hover table-condensed">
+						<thead>
+							<tr>
+								<th>Arete Siniga</th>
+								<th>Raza</th>
+								<th>Fecha de nacimiento</th>
+								<th>Fecha de compra</th>
+								<th>Fecha de venta</th>
+								<th>Acciones</th>
+							</tr>
+						</thead>
+						<tbody>
+						@foreach($cows as $cow)
+							<tr>
+								<td>{{ $cow->cattle->tag }}</td>
+								<td>{{ $cow->cattle->breed->name }}</td>
+								<td>{{ $cow->cattle->getBirthWithFormat() }}</td>
+								<td>{{ $cow->cattle->getPurchaseDateWithFormat() }}</td>
+								<td>
+									@if(count($cow->sale))
+									{{ $cow->sale->getSaleDateWithFormat() }}
+									@endif
+								</td>
+								<td>
+									@if(count($cow->sale) == 0)
+									<a class="btn btn-success btn-sm" href="{{ route('cows_sales.create', 'cow=' . $cow->id) }}" data-container="body" data-toggle="tooltip" data-placement="top" title="Crear registro de venta">
+										<span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+									</a>
+									@else
+									<a class="btn btn-info btn-sm" href="{{ route('cows_sales.show', $cow->id) }}" data-container="body" data-toggle="tooltip" data-placement="top" title="Mostrar información del registro">
+										<span class="glyphicon glyphicon-file" aria-hidden="true"></span>
+									</a>
+									<a class="btn btn-warning btn-sm" href="{{ route('cows_sales.edit', $cow->id) }}" data-container="body" data-toggle="tooltip" data-placement="top" title="Editar información del registro">
+										<span class="glyphicon glyphicon-open" aria-hidden="true"></span>
+									</a>
+									@endif
+								</td>
+							</tr>
+						@endforeach
+						</tbody>
+						</table>
 					</div>
+					<div>{{ $cows->links() }}</div>
+				@endif
 				</div>
+			</div>
 		</div>
 	</div>
 </div>
