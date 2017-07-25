566e2d4f224f39205426312267d4c8dea405c745
eduardorascon <eduardo.rascon@gmail.com>
2017-07-25 18:48:14 -0500

    Seeders classes added to the database seeder.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -8,12 +8,18 @@ class DatabaseSeeder extends Seeder
     {
     	$this->call(RoleTableSeeder::class);
         $this->call(UserTableSeeder::class);
-        $this->call(ClientsTableSeeder::class);
+        //catalogs
         $this->call(BreedsTableSeeder::class);
+        $this->call(ClientsTableSeeder::class);
+        $this->call(OwnersTableSeeder::class);
+        $this->call(PaddocksTableSeeder::class);
+        $this->call(VaccinesTableSeeder::class);
+        //data
         $this->call(CattleTableSeeder::class);
         $this->call(CowsTableSeeder::class);
+        $this->call(CalvesTableSeeder::class);
         $this->call(BullsTableSeeder::class);
-        $this->call(VaccinesTableSeeder::class);
-        
+        //logs
+        $this->call(WeightLogsTableSeeder::class);
     }
 }
