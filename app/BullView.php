64d9c0513427590ef8ae46411f9face6c5cf693b
eduardorascon <eduardo.rascon@gmail.com>
2017-07-26 13:25:51 -0500

    BullView model created.

---
+++ app/BullView.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+
+class BullView extends Model
+{
+    protected $table = 'bulls_view';
+	public $timestamps = false;
+}
