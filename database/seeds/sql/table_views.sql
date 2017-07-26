72205afcd08ad509cc3bb966f72f56f66a6dc484
eduardorascon <eduardo.rascon@gmail.com>
2017-07-26 13:20:43 -0500

    SQL sentence fixed.

--- database/seeds/sql/table_views.sql
+++ database/seeds/sql/table_views.sql
@@ -1,5 +1,5 @@
 create view bulls_view as
-select b.id, b.sale_id, c.tag, c.purchase_date, c.birth, c.is_alive, c.gender, br.name, o.name, p.name,
+select b.id, b.sale_id, c.tag, c.purchase_date, c.birth, c.is_alive, c.gender, br.name as breed_name, o.name as owner_name, p.name as paddock_name,
 (select weight from weight_logs where cattle_id = c.id order by date desc limit 1) as peso
 from bulls b
 inner join cattle c on b.cattle_id = c.id
@@ -8,7 +8,7 @@ left join owners o on c.owner_id = o.id
 left join paddocks p on c.paddock_id = p.id;
 
 create view cows_view as
-select co.id, co.sale_id, c.tag, c.purchase_date, c.birth, c.is_alive, c.gender, br.name, o.name, p.name,
+select co.id, co.sale_id, c.tag, c.purchase_date, c.birth, c.is_alive, c.gender, br.name as breed_name, o.name as owner_name, p.name as paddock_name,
 (select weight from weight_logs where cattle_id = c.id order by date desc limit 1) as peso
 from cows co
 inner join cattle c on co.cattle_id = c.id
