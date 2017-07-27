bcda0046b3a6e99c85128548925e8faa4756ef4c
eduardorascon <eduardo.rascon@gmail.com>
2017-07-27 12:39:24 -0500

    SQL for views creation updated.

--- database/seeds/sql/table_views.sql
+++ database/seeds/sql/table_views.sql
@@ -1,17 +1,29 @@
 create view bulls_view as
-select b.id, b.sale_id, c.tag, c.purchase_date, c.birth, c.is_alive, c.gender, br.name as breed_name, o.name as owner_name, p.name as paddock_name,
-(select weight from weight_logs where cattle_id = c.id order by date desc limit 1) as peso
+select
+	b.id, c.tag, c.purchase_date, c.birth, c.is_alive, c.gender,
+	br.id as breed_id, br.name as breed_name,
+	o.id as owner_id, o.name as owner_name,
+	p.id as paddock_id, p.name as paddock_name,
+	bs.id as sale_id, bs.sale_date,
+	(select weight from weight_logs where cattle_id = c.id order by date desc limit 1) as peso
 from bulls b
 inner join cattle c on b.cattle_id = c.id
 left join breeds br on c.breed_id = br.id
 left join owners o on c.owner_id = o.id
-left join paddocks p on c.paddock_id = p.id;
+left join paddocks p on c.paddock_id = p.id
+left join bulls_sales bs on b.sale_id = bs.id;
 
 create view cows_view as
-select co.id, co.sale_id, c.tag, c.purchase_date, c.birth, c.is_alive, c.gender, br.name as breed_name, o.name as owner_name, p.name as paddock_name,
-(select weight from weight_logs where cattle_id = c.id order by date desc limit 1) as peso
+select
+	co.id, c.tag, c.purchase_date, c.birth, c.is_alive, c.gender,
+	br.id as breed_id, br.name as breed_name,
+	o.id as owner_id, o.name as owner_name,
+	p.id as paddock_id, p.name as paddock_name,
+	cs.id as sale_id, cs.sale_date,
+	(select weight from weight_logs where cattle_id = c.id order by date desc limit 1) as peso
 from cows co
 inner join cattle c on co.cattle_id = c.id
 left join breeds br on c.breed_id = br.id
 left join owners o on c.owner_id = o.id
-left join paddocks p on c.paddock_id = p.id;
+left join paddocks p on c.paddock_id = p.id
+left join cows_sales cs on co.sale_id = cs.id;
