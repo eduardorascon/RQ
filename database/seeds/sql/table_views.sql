d79f5784f69aa4dc1453176469b485f1e5796570
eduardorascon <eduardo.rascon@gmail.com>
2017-07-27 17:29:50 -0500

    SQL for calves_view added.

--- database/seeds/sql/table_views.sql
+++ database/seeds/sql/table_views.sql
@@ -28,3 +28,19 @@ left join breeds br on c.breed_id = br.id
 left join owners o on c.owner_id = o.id
 left join paddocks p on c.paddock_id = p.id
 left join cows_sales cs on co.sale_id = cs.id;
+
+create view calves_view as
+select
+	ca.id, c.tag, co.id as mother_id, c.purchase_date, c.birth, c.is_alive, c.gender,
+	br.id as breed_id, br.name as breed_name,
+	o.id as owner_id, o.name as owner_name,
+	p.id as paddock_id, p.name as paddock_name,
+	cs.id as sale_id, cs.sale_date,
+	(select weight from weight_logs where cattle_id = c.id order by date desc limit 1) as peso
+from calves ca
+inner join cattle c on ca.cattle_id = c.id
+left join breeds br on c.breed_id = br.id
+left join owners o on c.owner_id = o.id
+left join paddocks p on c.paddock_id = p.id
+left join calves_sales cs on ca.sale_id = cs.id
+left join cows co on co.id = ca.cow_id;
