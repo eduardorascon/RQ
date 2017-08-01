fc786c18f71a3fc9f8408fd270bcaf53955762fb
eduardorascon <eduardo.rascon@gmail.com>
2017-08-01 18:19:04 -0500

    Export test deleted.

--- app/Http/Controllers/BullController.php
+++ app/Http/Controllers/BullController.php
@@ -18,7 +18,6 @@ use App\VaccineLog;
 use App\Picture;
 use Carbon\Carbon;
 use Khill\Lavacharts\Lavacharts;
-use Maatwebsite\Excel\Facades\Excel;
 
 class BullController extends Controller
 {
@@ -186,19 +185,5 @@ class BullController extends Controller
         }
 
         $chart = \Lava::LineChart('MyStocks', $stocksTable);
-    }
-
-    public function export_index()
-    {
-        Excel::create('Lista de Toros', function($excel) {
-            $excel->sheet('Toros', function($sheet) {
-                $bulls = Bull::select('cattle.*')->
-                join('cattle', 'bulls.cattle_id', '=', 'cattle.id')->
-                //leftJoin('bulls_sales', 'bulls.sale_id', '=', 'bulls_sales.id')->
-                orderBy('cattle.tag', 'asc')->get();
-
-                $sheet->fromArray($bulls);
-            });
-        })->export('xlsx');
     }
 }
