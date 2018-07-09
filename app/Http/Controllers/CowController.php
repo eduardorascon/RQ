b0a34d6cfbeae399d28cda388339602f8cf9db61
eduardorascon <eduardo.rascon@gmail.com>
2018-07-08 20:27:38 -0500

    Small refactoring.

--- app/Http/Controllers/CowController.php
+++ app/Http/Controllers/CowController.php
@@ -190,23 +190,18 @@ class CowController extends Controller
     public function save_picture(StorePictureRequest $request, $id)
     {
         $cow = Cow::findOrFail($id);
+        $pic = new Picture;
 
         if($request->hasFile('picture')) {
             $imageName = $cow->cattle_id . '-' . Carbon::now()->timestamp . '.' . $request->file('picture')->getClientOriginalExtension();
-            $request->file('picture')->move(base_path() . '/public/images/', $imageName);
-
-            $pic = new Picture;
             $pic->filename = $imageName;
-            $pic->comment = $request->comment;
-            $pic->cattle_id = $cow->cattle_id;
-            $pic->save();
+            
+            $request->file('picture')->move(base_path() . '/public/images/', $imageName);
         }
-        else {
-            $pic = new Picture;
-            $pic->comment = $request->comment;
-            $pic->cattle_id = $cow->cattle_id;
-            $pic->save();
-        }
+
+        $pic->comment = $request->comment;
+        $pic->cattle_id = $cow->cattle_id;
+        $pic->save();
 
         return redirect()->route('cows.show', $cow->id);
     }
