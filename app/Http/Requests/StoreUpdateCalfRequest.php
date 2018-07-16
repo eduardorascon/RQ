3beaf39b7eaf00cf005012f00c77e231a4c5aa96
eduardorascon <eduardo.rascon@gmail.com>
2018-07-15 19:49:10 -0500

    Validation added to make sure no duplicated values are created.

--- app/Http/Requests/StoreUpdateCalfRequest.php
+++ app/Http/Requests/StoreUpdateCalfRequest.php
@@ -15,6 +15,7 @@ class StoreUpdateCalfRequest extends FormRequest
     {
         return [
             'cow_id' => 'required',
+            'cattle_tag'=> 'required|unique:cattle,tag',
             'cattle_birth_date' => 'required',
             'cattle_breed' => 'required',
             'cattle_owner' => 'required',
@@ -27,6 +28,8 @@ class StoreUpdateCalfRequest extends FormRequest
     {
         return [
             'cow_id.required' => 'La madre del becerro es necesaria para el registro.',
+            'cattle_tag.required' => 'El arete siniga es necesario para el registro.',
+            'cattle_tag.unique' => 'El arete siniga ya fue utilizado en otro registro.',
             'cattle_birth_date.required' => 'La fecha de nacimiento es necesaria para el registro.',
             'cattle_breed.required' => 'La raza es necesaria para el registro.',
             'cattle_owner.required' => 'El dueño del becerro es necesario para el registro.',
--- app/Http/Requests/StoreUpdateCattleRequest.php
+++ app/Http/Requests/StoreUpdateCattleRequest.php
@@ -14,7 +14,7 @@ class StoreUpdateCattleRequest extends FormRequest
     public function rules()
     {
         return [
-            'cattle_tag'=> 'required',
+            'cattle_tag'=> 'required|unique:cattle,tag',
             'cattle_birth_date' => 'required',
             'cattle_purchase_date' => 'required',
             'cattle_breed' => 'required',
@@ -27,6 +27,7 @@ class StoreUpdateCattleRequest extends FormRequest
     {
         return [
             'cattle_tag.required' => 'El arete siniga es necesario para el registro.',
+            'cattle_tag.unique' => 'El arete siniga ya fue utilizado en otro registro.',
             'cattle_birth_date.required' => 'La fecha de nacimiento es necesaria para el registro.',
             'cattle_purchase_date.required' => 'La fecha de compra es necesaria para el registro.',
             'cattle_breed.required' => 'La raza es necesaria para el registro.',
