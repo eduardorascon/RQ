30cb7016504930973e3390a3c682e76343bd72de
Luis Eduardo Rascon Fierro <eduardo.rascon@gmail.com>
2018-07-18 09:49:29 -0500

    Tag and birthdate are the only two required values.

--- app/Http/Requests/UpdateCowRequest.php
+++ app/Http/Requests/UpdateCowRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreUpdateCowRequest extends FormRequest
+class UpdateCowRequest extends FormRequest
 {
 
     public function authorize()
@@ -15,15 +15,8 @@ class StoreUpdateCowRequest extends FormRequest
     public function rules()
     {
         return [
-            'cattle_tag'=> 'required|unique:cattle,tag',
-            'cattle_birth_date' => 'required',
-            'cattle_purchase_date' => 'required',
-            'cattle_breed' => 'required',
-            'cattle_owner' => 'required',
-            'cow_fertility' => 'required',
-            'cow_pregnancy_status' => 'required',
-            'cattle_is_alive' => 'required',
-            'cow_number_of_calves' => 'required'
+            'cattle_tag'=> 'required',
+            'cattle_birth_date' => 'required'
         ];
     }
 
@@ -31,15 +24,7 @@ class StoreUpdateCowRequest extends FormRequest
     {
         return [
             'cattle_tag.required' => 'El arete siniga es necesario para el registro.',
-            'cattle_tag.unique' => 'El arete siniga ya fue utilizado en otro registro.',
-            'cattle_birth_date.required' => 'La fecha de nacimiento es necesaria para el registro.',
-            'cattle_purchase_date.required' => 'La fecha de compra es necesaria para el registro.',
-            'cattle_breed.required' => 'La raza es necesaria para el registro.',
-            'cattle_owner.required' => 'El dueño del becerro es necesario para el registro.',
-            'cow_fertility.required' => 'La fertilidad de la vaca es necesaria para el registro.',
-            'cow_pregnancy_status.required' => 'El estatus de gestación es necesario para el registro.',
-            'cattle_is_alive.required' => 'El estatus ¿Esta vivo? es necesario para el registro.',
-            'cow_number_of_calves.required' => 'El numero de becerros es necesario para el registro.'
+            'cattle_birth_date.required' => 'La fecha de nacimiento es necesaria para el registro.'
         ];
     }
 }
